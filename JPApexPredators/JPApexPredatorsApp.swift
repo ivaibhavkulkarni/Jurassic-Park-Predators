7f694f12ad1e24f65f406550684efbd84d0b977c
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-03-07 10:01:30 +0530

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Vaibhav kulkarni on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
