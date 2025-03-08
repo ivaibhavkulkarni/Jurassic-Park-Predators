7e25255d1896c757aecd3810fdddccd2bb7a6c48
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-03-08 20:26:40 +0530

    filtering the list

---
+++ JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,14 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by Vaibhav kulkarni on 08/03/25.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text("Predator Detail")
+    }
+}
